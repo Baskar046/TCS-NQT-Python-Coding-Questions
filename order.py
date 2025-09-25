dd154949a524d1b8af5acbbc96d5bdfd297650eb
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-09-25 12:34:38 +0530

    Update order.py

--- order.py
+++ order.py
@@ -1,3 +1,14 @@
+"""
+Move Zeros to End
+
+Description: Given an integer array, move all zeros to the end while preserving the order of non-zero elements.
+
+Input: n (number of elements), then array of size n.
+
+Output: Modified array.
+"""
+
+
 n=[0,1,2,3,0]
 zero=[]
 res=[]
